e862651c23ce27ba07ecacb3d2e1f3bdbb8436eb
Manarox <57014646+Manarox@users.noreply.github.com>
2022-02-17 08:37:57 +0000

    Deploying to gh-pages from @ Manarox/training-shop@9e191fefccb96a3b1586e91cae1099a242aab335 🚀

---
+++ precache-manifest.f5dd02aaabf0390dd00b2b50995eeb08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8a891360047f0e1198abeb6420a7f06",
+    "url": "/training-shop/index.html"
+  },
+  {
+    "revision": "54f9d302f8979d59e841",
+    "url": "/training-shop/static/css/main.ed2094bf.chunk.css"
+  },
+  {
+    "revision": "30fdbfac04d62dea000a",
+    "url": "/training-shop/static/js/2.ce863237.chunk.js"
+  },
+  {
+    "revision": "440f8641b4ac1935df7a",
+    "url": "/training-shop/static/js/3.76c3d8c6.chunk.js"
+  },
+  {
+    "revision": "54f9d302f8979d59e841",
+    "url": "/training-shop/static/js/main.09434cb6.chunk.js"
+  },
+  {
+    "revision": "fe4454923b81a7633fa6",
+    "url": "/training-shop/static/js/runtime~main.3bf2f39d.js"
+  }
+]);

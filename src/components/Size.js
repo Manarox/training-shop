f3d885caaedd1b69d40f96693a0d2f28bad3b55c
dmitrii starikov <manaroxik@yandex.ru>
2022-03-10 18:28:32 +0300

    zzz

--- src/components/Size.js
+++ src/components/Size.js
@@ -3,29 +3,40 @@ import React, { useEffect, useState } from 'react';
 const Size = (props) => {
     const {product} = props;
 
-    let [btnContent, setBtnContent] = useState({product}.product.sizes[0]);
+    // let [btnContent, setBtnContent] = useState({product}.product.sizes[0]);
+
+    // useEffect(() => {
+    //     const ite = document.querySelectorAll(".size__btn");
+    //     ite[0].classList.add("size__btn_active");
+    //   }, []);
+
+    // function sizeChose(e) {
+    //     const items = document.querySelectorAll(".size__btn");
+    //     const target = e.currentTarget;
+    //     Array.from(items).forEach((item) => {
+    //         item.classList.remove("size__btn_active");
+    //     });
+    //     target.classList.add("size__btn_active");
+    //     setBtnContent((btnContent = e.currentTarget.textContent))
+    // }
+    // console.log(`arr`, {product}.product.sizes)
+
+    const [sizee, setSize] = useState({product}.product.sizes[0]);
+    const changeSize = (e) => {
+        setSize(e.target.value);
+      };
+    let gfgfgf = {product}.product.sizes[0]
 
     useEffect(() => {
-        const ite = document.querySelectorAll(".size__btn");
-        ite[0].classList.add("size__btn_active");
-      }, []);
+        setSize(gfgfgf);
+    }, [gfgfgf]);
 
-    function sizeChose(e) {
-        const items = document.querySelectorAll(".size__btn");
-        const target = e.currentTarget;
-        Array.from(items).forEach((item) => {
-            item.classList.remove("size__btn_active");
-        });
-        target.classList.add("size__btn_active");
-        setBtnContent((btnContent = e.currentTarget.textContent))
-    }
-    // console.log(`arr`, {product}.product.sizes)
 
     return (
         <>
         <div class="size">
             <div class="size__title">
-                <span class="color__text">size:</span><span class="color__span">{btnContent}</span>
+                <span class="color__text">size:</span><span class="color__span">{sizee}</span>
             </div>
             <ul class="size__image-list flex list-reset">
 
@@ -33,7 +44,7 @@ const Size = (props) => {
                     return (
 
                         <li className="size__item">
-                            <button className='size__btn' onClick={sizeChose}>{post}</button>
+                            <button value={post} className={post === sizee ? 'size__btn size__btn_active' : 'size__btn'} onClick={(e) => changeSize(e)}>{post}</button>
                         </li>
 
                     )

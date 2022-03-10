96eac94c1fc4ef4e35505d48073ee6f43d93f71f
dmitrii starikov <manaroxik@yandex.ru>
2022-03-10 18:21:20 +0300

    zzz

--- src/components/Color.js
+++ src/components/Color.js
@@ -12,33 +12,44 @@ const Color = (props) => {
 
     console.log(result)
 
-    let [btnContent, setBtnContent] = useState(result[0].color);
+    // let [btnContent, setBtnContent] = useState(result[0].color);
 
-    // console.log(result[0].color)
+    // // console.log(result[0].color)
 
     
+    // useEffect(() => {
+    //     const ite = document.querySelectorAll(".color__image");
+    //     ite[0].classList.add("color__image_active");
+    //   },)
+
+    // function colorChose(e) {
+    //     const items = document.querySelectorAll(".color__image");
+    //     const target = e.currentTarget;
+    //     Array.from(items).forEach((item) => {
+    //         item.classList.remove("color__image_active");
+    //     });
+    //     target.classList.add("color__image_active");
+    //     setBtnContent((e.currentTarget.id))
+    // }
+
+    const [colorr, setColor] = useState(result[0].color);
+    let gfgfgf = result[0].color
+
+    const changeImage = (e) => {
+
+        setColor(e.currentTarget.id);
+    };
+
     useEffect(() => {
-        const ite = document.querySelectorAll(".color__image");
-        ite[0].classList.add("color__image_active");
-      },)
-
-    function colorChose(e) {
-        const items = document.querySelectorAll(".color__image");
-        const target = e.currentTarget;
-        Array.from(items).forEach((item) => {
-            item.classList.remove("color__image_active");
-        });
-        target.classList.add("color__image_active");
-        setBtnContent((e.currentTarget.id))
-    }
-
+        setColor(gfgfgf);
+    }, [gfgfgf]);
 
 
     return (
         <>
         <div class="color">
             <div class="color__title">
-                <span class="color__text">Color:</span><span class="color__span">{btnContent}</span>
+                <span class="color__text">Color:</span><span class="color__span">{colorr}</span>
             </div>
 
             <ul class="color__image-list flex list-reset">
@@ -47,7 +58,7 @@ const Color = (props) => {
                     return (
 
                     <li class="color__item">
-                        <img src={"https://training.cleverland.by/shop" + post.url} className="color__image" alt="Rating product" onClick={colorChose} id={post.color}/>
+                        <img src={"https://training.cleverland.by/shop" + post.url} className={colorr === post.color ? 'color__image color__image_active' : 'color__image'} alt="Rating product" onClick={(e) => changeImage(e)} id={post.color}/>
                     </li>
 
                     )
